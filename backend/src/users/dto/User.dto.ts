f5b76ddd3102a891392198ff34f47325d4f5fe79
gazzaar <gazzarDev@gmail.com>
2025-09-21 15:50:20 +0300

    user basic DTO

---
+++ backend/src/users/dto/User.dto.ts
@@ -0,0 +1,22 @@
+import { Expose, Transform } from 'class-transformer';
+export class UserDto {
+  @Expose()
+  id: string;
+
+  @Expose()
+  fullName: string;
+
+  @Expose()
+  email: string;
+
+  @Expose()
+  password: string;
+
+  @Expose()
+  @Transform(({ value }) => value.getTime())
+  createdAt: number;
+
+  @Expose()
+  @Transform(({ value }) => value.getTime())
+  updatedAt: number;
+}
--- backend/src/users/dto/createUser.dto.ts
+++ backend/src/users/dto/createUser.dto.ts
@@ -1,7 +1,14 @@
+import { IsString, IsNotEmpty } from 'class-validator';
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   fullName: string;
+
+  @IsString()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
-
-export default CreateUserDto;
