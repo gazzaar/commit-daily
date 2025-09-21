6f3bc38ce3c9927901c3b9c707261ea7ca5ec01d
gazzaar <gazzarDev@gmail.com>
2025-09-21 15:50:01 +0300

    prisma migrations

---
+++ backend/prisma/migrations/20250921095034_user/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" UUID NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
