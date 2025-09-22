2adc3d9da5ff6afbc37b7e8f2644aaefee9d346e
gazzaar <gazzarDev@gmail.com>
2025-09-22 22:24:48 +0300

    configure jwt

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthenticationModule } from './authentication/authentication.module';
+import * as Joi from '@hapi/joi';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, AuthenticationModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION_TIME: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    AuthenticationModule,
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
