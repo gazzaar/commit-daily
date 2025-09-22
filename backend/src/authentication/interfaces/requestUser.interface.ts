ad129993cbc574b29ca16e95f933962e03116ce6
gazzaar <gazzarDev@gmail.com>
2025-09-22 22:25:31 +0300

    Add reqeust user interface

--- backend/src/authentication/interfaces/requestUser.interface.ts
+++ backend/src/authentication/interfaces/requestUser.interface.ts
@@ -1,6 +1,7 @@
 import { Request } from 'express';
 import { User } from 'src/users/interfaces/user.interface';
 
-export interface requestUser extends Request {
-  user: Pick<User, 'email' | 'password'>;
+type SafeUser = Omit<User, 'password'>;
+export interface RequestUser extends Request {
+  user: SafeUser;
 }
