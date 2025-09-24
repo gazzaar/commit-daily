a4d6dfa4d94a02231a3cc2b24cda3bf910027f13
gazzaar <gazzarDev@gmail.com>
2025-09-24 20:32:15 +0300

    basic habits operations

--- backend/src/habits/habits.module.ts
+++ backend/src/habits/habits.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { HabitsController } from './habits.controller';
+import { HabitsResolver } from './habits.resolver';
 import { HabitService } from './habits.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [HabitService],
-  controllers: [HabitsController],
+  providers: [HabitService, HabitsResolver],
 })
 export class HabitsModule {}
