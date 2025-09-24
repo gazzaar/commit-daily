0bc7fa859c530aae0fc33e13aeb9acfb08610394
gazzaar <gazzarDev@gmail.com>
2025-09-24 20:31:14 +0300

    configure GraphQL

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,28 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthenticationModule } from './authentication/authentication.module';
 import * as Joi from '@hapi/joi';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { HabitsModule } from './habits/habits.module';
 
 @Module({
   imports: [
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        playground: Boolean(configService.get('GRAPHQL_PLAYGROUND')),
+        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+        context: ({ req, res }) => ({ req, res }),
+        cors: {
+          origin: 'http://localhost:3000',
+          credentials: true,
+        },
+      }),
+    }),
     ConfigModule.forRoot({
       validationSchema: Joi.object({
         JWT_SECRET: Joi.string().required(),
@@ -15,6 +33,7 @@ import { ConfigModule } from '@nestjs/config';
     }),
     AuthenticationModule,
     UsersModule,
+    HabitsModule,
   ],
   controllers: [],
   providers: [],
