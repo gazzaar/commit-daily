a02d4e626f8f4dc019ff61929698bb2651370525
gazzaar <gazzarDev@gmail.com>
2025-09-27 08:53:02 +0300

    GraqhQL integration after suffering

---
+++ web/codegen.ts
@@ -0,0 +1,15 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: '../backend/src/schema.gql',
+  documents: ['src/**/*.tsx', '!src/gql/**/*'],
+  ignoreNoDocuments: true, // for better experience with the watcher
+  generates: {
+    './src/gql/': {
+      preset: 'client',
+      plugins: [],
+    },
+  },
+};
+
+export default config;
