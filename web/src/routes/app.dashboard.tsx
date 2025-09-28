8ab1803681cd315cd5e1baf8f66f58064191d2ad
gazzaar <gazzarDev@gmail.com>
2025-09-29 02:34:03 +0300

    dashboard route

--- web/src/routes/app.dashboard.tsx
+++ web/src/routes/app.dashboard.tsx
@@ -1,9 +1,39 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { useHabits, useToggleHabit } from '../hooks/habits';
+import HabitItem from '../components/HabitItem/HabitItem';
 
 export const Route = createFileRoute('/app/dashboard')({
-  component: RouteComponent,
+  component: Dashboard,
 });
 
-function RouteComponent() {
-  return <div>Hello "/app/dashboard"!</div>;
+function Dashboard() {
+  const { data, isLoading, error } = useHabits();
+  const toggleHabit = useToggleHabit();
+
+  const handleToggleHabit = async (habitId: string) => {
+    try {
+      const date = new Date().toISOString().substring(0, 10);
+      await toggleHabit.mutateAsync({
+        habitId,
+        date,
+      });
+    } catch (err) {}
+  };
+
+  if (isLoading) return <h1>...</h1>;
+  if (error) return <div> Error {(error as Error).message}</div>;
+
+  return (
+    <>
+      {data?.habits?.map((h) => (
+        <HabitItem habitData={h} key={h.id}>
+          <input
+            type='checkbox'
+            checked={h.completedToday}
+            onChange={() => handleToggleHabit(h.id)}
+          />
+        </HabitItem>
+      ))}
+    </>
+  );
 }
