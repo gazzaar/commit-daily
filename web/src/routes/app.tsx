0502f5479b3d2828280ecbe64909dcea7131dcc8
gazzaar <gazzarDev@gmail.com>
2025-09-29 02:33:43 +0300

    app layout

--- web/src/routes/app.tsx
+++ web/src/routes/app.tsx
@@ -1,16 +1,19 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { Outlet } from '@tanstack/react-router';
 import Header from '../components/header/Header';
+import { ProtectedRoute } from '../components/ProtectedRoute';
 
 export const Route = createFileRoute('/app')({
-  component: RouteComponent,
+  component: AppLayout,
 });
 
-function RouteComponent() {
+function AppLayout() {
   return (
     <>
-      <Header />
-      <Outlet />
+      <ProtectedRoute>
+        <Header />
+        <Outlet />
+      </ProtectedRoute>
     </>
   );
 }
