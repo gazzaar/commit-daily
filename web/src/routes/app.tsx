02feabf9e889297c22c2a9e1b6d4ac0e9633faa1
gazzaar <gazzarDev@gmail.com>
2025-09-30 22:40:33 +0300

    redirect /app/

--- web/src/routes/app.tsx
+++ web/src/routes/app.tsx
@@ -1,9 +1,16 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 import { Outlet } from '@tanstack/react-router';
 import Header from '../components/Header';
 import { ProtectedRoute } from '../components/ProtectedRoute';
 
 export const Route = createFileRoute('/app')({
+  beforeLoad: ({ location }) => {
+    if (location.pathname === '/app' || location.pathname === '/app/') {
+      throw redirect({
+        to: '/app/dashboard',
+      });
+    }
+  },
   component: AppLayout,
 });
 
