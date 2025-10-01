a0841a25c21f695f58e1bcc461dfcfe4c174490d
gazzaar <gazzarDev@gmail.com>
2025-10-01 04:16:35 +0300

    Update app.tsx

--- web/src/routes/app.tsx
+++ web/src/routes/app.tsx
@@ -1,13 +1,13 @@
-import { createFileRoute, redirect } from '@tanstack/react-router';
-import { Outlet } from '@tanstack/react-router';
-import Header from '../components/Header';
-import { ProtectedRoute } from '../components/ProtectedRoute';
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import { Outlet } from "@tanstack/react-router";
+import Header from "../components/Header";
+import { ProtectedRoute } from "../components/ProtectedRoute";
 
-export const Route = createFileRoute('/app')({
+export const Route = createFileRoute("/app")({
   beforeLoad: ({ location }) => {
-    if (location.pathname === '/app' || location.pathname === '/app/') {
+    if (location.pathname === "/app" || location.pathname === "/app/") {
       throw redirect({
-        to: '/app/dashboard',
+        to: "/app/dashboard",
       });
     }
   },
@@ -19,7 +19,9 @@ function AppLayout() {
     <>
       <ProtectedRoute>
         <Header />
-        <Outlet />
+        <main className="wrapper">
+          <Outlet />
+        </main>
       </ProtectedRoute>
     </>
   );
