5d5c2910189e4b943915e3c6f1e2f8344b46b4aa
gazzaar <gazzarDev@gmail.com>
2025-10-02 04:31:17 +0300

    basic dialog

--- web/src/routes/app.manage-habits.tsx
+++ web/src/routes/app.manage-habits.tsx
@@ -1,6 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { useCreateHabit, useDeleteHabit, useHabits } from '../hooks/habits';
 import HabitItem from '../components/HabitItem/HabitItem';
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import { TbEdit } from 'react-icons/tb';
+import { useRef, useState } from 'react';
 
 export const Route = createFileRoute('/app/manage-habits')({
   component: HabitManageMent,
@@ -10,12 +13,19 @@ function HabitManageMent() {
   const { data, isLoading, error } = useHabits();
   const deleteHabit = useDeleteHabit();
   const createHabit = useCreateHabit();
+  const dialogRef = useRef<HTMLDialogElement | null>(null);
+
+  const openDialog = () => {
+    if (dialogRef.current) dialogRef.current.showModal();
+  };
+
+  const closeDialog = () => {
+    if (dialogRef.current) dialogRef.current.close();
+  };
 
   const handleDeleteHabit = async (habitId: string) => {
     try {
-      if (confirm('Are you sure?')) {
-        await deleteHabit.mutateAsync(habitId);
-      }
+      await deleteHabit.mutateAsync(habitId);
     } catch (error) {
       console.error(error);
     }
@@ -35,11 +45,8 @@ function HabitManageMent() {
         habitName: habitName.trim(),
         description: description.trim(),
       });
-
-      // Reset form
-      e.currentTarget.reset();
     } catch (error) {
-      console.error('Failed to create habit');
+      console.error('Failed to create habit', error);
     }
   };
 
@@ -48,27 +55,50 @@ function HabitManageMent() {
 
   return (
     <>
-      {data?.habits.map((habit) => (
-        <HabitItem habitData={habit} key={habit.id}>
-          <button onClick={() => handleDeleteHabit(habit.id)}>
-            Delete - X{' '}
-          </button>
-        </HabitItem>
-      ))}
-      <div>
-        <form onSubmit={handleCreateHabit}>
-          <div>
+      <button onClick={() => openDialog()}>Add new habit</button>
+      <div className='habits'>
+        {data?.habits.map((habit) => (
+          <HabitItem habitData={habit} key={habit.id}>
+            {() => (
+              <>
+                <button className='btn-edit'>
+                  {' '}
+                  <span>
+                    <TbEdit />
+                  </span>{' '}
+                  Edit
+                </button>
+                <button
+                  className='btn btn-delete'
+                  onClick={() => handleDeleteHabit(habit.id)}
+                >
+                  <RiDeleteBin6Line />
+                </button>
+              </>
+            )}
+          </HabitItem>
+        ))}
+      </div>
+      <dialog id='dialog' ref={dialogRef} className='dialog'>
+        <button id='close' onClick={closeDialog}>
+          Close
+        </button>
+        <form className='form-add-habit' onSubmit={handleCreateHabit}>
+          <div className='input-container'>
             <label> Habit Name</label>
             <input type='text' name='habitName' required />
           </div>
 
-          <div>
+          <div className='input-container'>
             <label> description</label>
             <input type='text' name='description' required />
           </div>
-          <button type='submit'> Create Habit</button>
+          <button className='btn' type='submit'>
+            {' '}
+            Create Habit
+          </button>
         </form>
-      </div>
+      </dialog>
     </>
   );
 }
