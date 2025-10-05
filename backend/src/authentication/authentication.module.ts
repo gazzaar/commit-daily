3431179d972eef81eb83ca3f55f36a21d356a6f2
gazzaar <gazzarDev@gmail.com>
2025-10-05 10:53:23 +0300

    fix useFactory async

--- backend/src/authentication/authentication.module.ts
+++ backend/src/authentication/authentication.module.ts
@@ -16,7 +16,7 @@ import { JwtStrategy } from './jwt.strategy';
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
+      useFactory: (configService: ConfigService) => ({
         secret: configService.get('JWT_SECRET'),
         signOptions: {
           expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}`,
